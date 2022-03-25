4449ef54da392ba004af14fcf36f3332256399cc
gabriegsti <gabrielgsdevbr@gmail.com>
2022-03-25 11:59:54 -0300

    bytebank 2 resolvido problema com main

---
+++ ByteBank2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(0, 0);
+
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Console.ReadLine();
+        }
+    }
+}
